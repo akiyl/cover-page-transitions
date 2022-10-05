490fc180343d6f2066ecbbc665acd20bf3a72682
akiyl <akiyl>
2022-10-05 11:19:19 +0530

    media query added

---
+++ README.md
@@ -0,0 +1,4 @@
+# cover_page_transition
+This is a practice project prepared by me.
+Heres the orignal site =>
+https://tympanus.net/Tutorials/CoverPageTransition/ 
---
+++ app.js
@@ -0,0 +1,136 @@
+// import { animate } from "./motion"
+
+// Animate via selector
+
+// import '../styles/style.scss'
+
+const preview_2021 = document.querySelector('.preview_2021');
+const preview_2020 = document.querySelector('.preview_2020');
+const preview_2022 = document.querySelector('.preview_2022');
+const img_inner = document.querySelectorAll('.preview_image_inner');
+const preview = document.querySelector('.previews');
+const frame = document.querySelector('.frame')
+const ancor = document.querySelectorAll('a')
+    // const frame_style = document.querySelector('.frame_styles');
+const logo = document.querySelector('.logo_aroww');
+const logo_bennet = document.getElementById('logo_aroww_bennet');
+const logo_hughes = document.getElementById('logo_aroww_hughes');
+const hover = document.querySelector('.hov');
+const hover2 = document.querySelector('.hov2');
+const hover3 = document.querySelector('.hov3');
+const image2 = document.querySelector('.item_image_inner2');
+const clk = document.getElementById('click2')
+const clk3 = document.getElementById('click3')
+const clk4 = document.getElementById('click4')
+const image = document.querySelector('.item_image_inner');
+const image3 = document.querySelector('.item_image_inner3');
+const overlay_top = document.querySelector('.overlay_screen');
+const overlay_bottom = document.querySelector('.overlay_screen2');
+const child = preview.children;
+// let pris = hover2.parentElement.children[2];
+// console.log(hover2.parentElement.children[2].children)
+let preview_open = (element) => {
+    element.style.opacity = ('1')
+    frame.classList.add('frame_styles')
+        // frame_style.style.transform = ('translateY(0px)')
+    element.style.pointerEvents = ('auto')
+    overlay_top.style.transform = ('translateY(500px)');
+    overlay_bottom.style.transform = ('translateY(-500px)');
+    preview.style.transitionDelay = ('1.5s')
+    preview.style.opacity = ('1');
+    preview.style.pointerEvents = ('auto');
+    // frame.style.color = ('#a17445')
+
+    element.style.transform = ('translateY(100px)')
+    console.log(img_inner)
+    ancor.forEach(e =>{
+        e.style.color = "#a17445"
+    })
+    img_inner.forEach(e => {
+        e.style.transform = ('translateY(0px)')
+
+    })
+}
+let preview_close = (element) => {
+    element.style.opacity = ('0')
+    element.style.pointerEvents = ('none')
+    frame.classList.remove('frame_styles');
+    overlay_top.style.transform = ('translateY(-500px)');
+    overlay_bottom.style.transform = ('translateY(500px)');
+    overlay_top.style.transitionDelay = ('1s')
+    overlay_bottom.style.transitionDelay = ('1s')
+    preview.style.opacity = ('0');
+    preview.style.pointerEvents = ('none');
+    // frame.style.color = ('#000')
+    preview.style.transform = ('translateY(-100px)')
+    ancor.forEach(e =>{
+        e.style.color = "CurrentColor"
+    })
+    img_inner.forEach(e => {
+        e.style.transform = ('translateY(270px)')
+
+    })
+
+    preview.style.transitionDelay = ('0s')
+
+}
+logo.addEventListener('click', () => {
+
+    preview_close(preview_2020)
+
+
+})
+clk.addEventListener('click', () => {
+
+    preview_open(preview_2020)
+})
+clk3.addEventListener('click', () => {
+
+    preview_open(preview_2021)
+})
+logo_bennet.addEventListener('click', () => {
+
+    preview_close(preview_2021)
+})
+clk4.addEventListener('click', () => {
+    preview_open(preview_2022)
+})
+logo_hughes.addEventListener('click', () => {
+    preview_close(preview_2022)
+})
+
+
+hover2.addEventListener('mouseover', () => {
+
+    scalein(image2)
+
+
+})
+hover2.addEventListener('mouseout', () => {
+    scaleout(image2)
+})
+hover3.addEventListener('mouseover', () => {
+
+    scalein(image3)
+
+
+})
+hover3.addEventListener('mouseout', () => {
+    scaleout(image3)
+})
+hover.addEventListener('mouseover', () => {
+
+    scalein(image)
+
+})
+hover.addEventListener('mouseout', () => {
+    scaleout(image)
+})
+let scalein = (element) => {
+    element.style.transform = 'scale(1.2)'
+    element.style.transition = '1s ease'
+}
+let scaleout = (element) => {
+    element.style.transform = 'scale(1)'
+    element.style.transition = '0.5s ease'
+}
